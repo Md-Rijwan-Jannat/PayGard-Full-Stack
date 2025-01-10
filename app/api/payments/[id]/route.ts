d85b4c8fe747393e89436658c8523dd0ccebd0fb
Md Rijwan Jannat <rijwanjannat36@gmail.com>
2025-01-10 14:08:52 +0600

    create payments apis

---
+++ app/api/payments/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import authOptions from "@/app/api/auth/[...nextauth]/route";
+import { Payment } from "@/models";
+import mongoose from "mongoose";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const session: { user?: { role?: string } } | null = await getServerSession(
+    authOptions
+  );
+  if (!session || !session.user || session.user.role !== "admin") {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { status } = await req.json();
+
+  await mongoose.connect(process.env.MONGODB_URI!);
+  const payment = await Payment.findByIdAndUpdate(
+    params.id,
+    { status },
+    { new: true }
+  );
+
+  return NextResponse.json(payment);
+}
